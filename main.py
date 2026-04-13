1a0a931685b72955b3f6f2484f0c6012c4d2e68d
forlorn <andrey.paryev@yandex.ru>
2026-04-14 00:06:20 +0300

    Созданы файлы main, solver, utils. Подбор чисел и жадный алгоритм реализованы.

---
+++ main.py
@@ -0,0 +1,47 @@
+from utils import *
+from solver import *
+import time
+
+w = 1 # Емкость рюкзака
+
+n = int(input("Введите количество предметов>>"))
+x = int(input("Введите максимальную стоимость предмета>>"))
+
+# Списки-словари для равномерного и нормального распределений
+itemsUniform = uniform_distribution(n, x)
+itemsNormal = normal_distribution(n, x)
+
+'''choiseVar = input()
+
+textVar = {
+    "1": "равномерного",
+    "2": "нормального"
+}
+var = {
+    "1": itemsUniform,
+    "2": itemsNormal
+}'''
+
+#print(f"Равномерное распределение {itemsUniform}")
+#print(f"Нормальное распределение {itemsNormal}")
+
+#Жадный для равномерного
+start = time.perf_counter()
+sorted_items, total_v, total_cost = greedy_algo(itemsUniform, w)
+end = time.perf_counter()
+print("Жадный алгоритм для равномерного распределения:")
+print("\n".join(f"Индекс: {i['index']}, Вес: {i['weight']:.5f}, Цена: {i['price']:.5f}" for i in sorted_items))
+print(f"\nИтоговый вес: {total_v}")
+print(f"Итоговая стоимость: {total_cost}\n")
+print(f"Выполнено за: {end - start:.8f} сек.")
+
+
+#Жадный для нормального
+start = time.perf_counter()
+sorted_items, total_v, total_cost = greedy_algo(itemsNormal, w)
+end = time.perf_counter()
+print("Жадный алгоритм для нормального распределения:")
+print("\n".join(f"Индекс: {i['index']}, Вес: {i['weight']:.5f}, Цена: {i['price']:.5f}" for i in sorted_items))
+print(f"\nИтоговый вес: {total_v}")
+print(f"Итоговая стоимость: {total_cost}\n")
+print(f"Выполнено за: {end - start:.8f} сек.")
---
+++ utils.py
@@ -0,0 +1,25 @@
+import random
+
+# Равномерное распределение от 0 до 1  
+def uniform_distribution(n, x):
+    weight_cost = []
+    for i in range(n):
+        weight_cost.append({"index":i, "weight": random.uniform(0, 1),
+                             "price": items_cost(x)})  
+    return weight_cost
+
+# Нормальное распределение с математическим ожиданием 0.5 и стандартным отклонением 0.4
+def normal_distribution(n, x):
+    weight_cost = []
+    mat_expectation = 0.5
+    std_dev = 0.4
+    while len(weight_cost) < n:
+        weightN = random.gauss(mat_expectation, std_dev)
+        if 0 < weightN < 1:
+            weight_cost.append({"index": len(weight_cost),"weight": weightN,
+                                 "price": items_cost(x)})
+    return weight_cost
+
+# Стоимость отдельного предмета
+def items_cost(x):
+    return random.uniform(1, x)
