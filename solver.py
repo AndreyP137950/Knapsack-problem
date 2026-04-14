a3af98be2bf77f7ff17d1aac93b575cd7f695a76
hekemi <vaonmax@gmail.com>
2026-04-14 11:48:02 +0300

    Сделал набросок метода ветвей и границ

--- solver.py
+++ solver.py
@@ -25,4 +25,22 @@ def greedy_algo(Lst, w):
 
 # Метод ветвей и границ
 def branch_boundary_method(Lst, w):
-    return 0
+    # Подготовка массива к использованию
+    cLst = Lst.copy()
+    
+    # список в который посчитаем отношение цена/вес
+    cooked_list = []
+
+    # здесь его и считаем
+    for item in cLst:
+        ratio = float("inf") if item["weight"] == 0 else item["price"] / item["weight"]
+        cooked_list.append((item, ratio)) # на выход список кортежей (элемент, отношение цена/вес)
+    
+    # отсортировали по убыванию отношения цена/вес
+    cooked_list.sort(key=lambda x: x[1], reverse=True)
+    
+    rLts = []
+    v = cost = 0
+    
+
+    return rLts, v, cost
